8cf388a9ec463f32a4226a9a0f2a6091d6ce1d52
marborkowska <92572696+marborkowska@users.noreply.github.com>
2022-03-21 12:32:20 +0100

    changes

--- docs/oxygen-webhelp/app/config.js
+++ docs/oxygen-webhelp/app/config.js
@@ -110,7 +110,7 @@ define(function() {
         	
             if(id === 'index-1' || id === 'index-2' || id === 'index-3' || id === 'stopwords' 
             ||id === 'htmlFileInfoList' || id === 'keywords') {
-                args = '20220321121954';
+                args = '20220321123146';
         	} 
 	
 	        return (url.indexOf('?') === -1 ? '?' : '&') + args;
