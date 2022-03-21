d53f0661f4ec3ed4d70d47fe139586c0586094d1
marborkowska <92572696+marborkowska@users.noreply.github.com>
2022-03-21 12:45:27 +0100

    changes

--- docs/oxygen-webhelp/app/search/index/keywords.js
+++ docs/oxygen-webhelp/app/search/index/keywords.js
@@ -1,8 +1,7 @@
-define(function() {var keywords=[{w:"My",p:["p0","p1","p2","p4","p5","p6","p7","p8","p9","p11"]},{w:"first",p:["p0","p4","p7","p11"]},{w:"topic",p:["p0","p1","p2","p4","p10"]},{w:"second",p:["p1","p5","p8"]},{w:"third",p:["p2","p6","p9"]},{w:"Glossary",p:["p3"]},{w:"reference",p:["p4","p5","p6"]},{w:"Creating",p:["p7","p8","p9"]},{w:"task",p:["p7","p8","p9"]},{w:"New",p:["p10"]},{w:"map",p:["p11"]}];
+define(function() {var keywords=[{w:"My",p:["p0","p1","p2","p4","p5","p6","p7","p8","p9","p11","p12"]},{w:"first",p:["p0","p4","p7","p12"]},{w:"topic",p:["p0","p1","p2","p4","p10"]},{w:"second",p:["p1","p5","p8"]},{w:"third",p:["p2","p6","p9"]},{w:"Glossary",p:["p3"]},{w:"reference",p:["p4","p5","p6"]},{w:"Creating",p:["p7","p8","p9"]},{w:"task",p:["p7","p8","p9"]},{w:"New",p:["p10"]},{w:"DITA",p:["p11"]},{w:"map",p:["p11","p12"]}];
 var ph={};
 ph["p0"]=[0, 1, 2];
 ph["p1"]=[0, 3, 2];
-ph["p11"]=[0, 1, 10];
 ph["p2"]=[0, 4, 2];
 ph["p3"]=[5];
 ph["p4"]=[0, 1, 6, 2];
@@ -12,6 +11,8 @@ ph["p7"]=[7, 0, 1, 8];
 ph["p8"]=[7, 0, 3, 8];
 ph["p9"]=[7, 0, 4, 8];
 ph["p10"]=[9, 2];
+ph["p12"]=[0, 1, 11];
+ph["p11"]=[0, 10, 11];
      return {
          keywords: keywords,
          ph: ph
