1a834c3653b30af499b26d24fb475186f6cfa556
marborkowska <92572696+marborkowska@users.noreply.github.com>
2022-03-24 13:43:55 +0100

    structure + first chapter

--- docs/oxygen-webhelp/app/config.js
+++ docs/oxygen-webhelp/app/config.js
@@ -110,7 +110,7 @@ define(function() {
         	
             if(id === 'index-1' || id === 'index-2' || id === 'index-3' || id === 'stopwords' 
             ||id === 'htmlFileInfoList' || id === 'keywords') {
-                args = '20220322165854';
+                args = '20220324115611';
         	} 
 	
 	        return (url.indexOf('?') === -1 ? '?' : '&') + args;
--- docs/oxygen-webhelp/app/search/index/keywords.js
+++ docs/oxygen-webhelp/app/search/index/keywords.js
@@ -1,6 +1,43 @@
-define(function() {var keywords=[{w:"Introduction",p:["p0"]}];
+define(function() {var keywords=[{w:"Alignment",p:["p0"]},{w:"Analytics",p:["p1"]},{w:"Automatic",p:["p2"]},{w:"QA",p:["p2"]},{w:"Best",p:["p3"]},{w:"cooperation",p:["p3"]},{w:"practices",p:["p3"]},{w:"Cloud",p:["p4","p23"]},{w:"vs.",p:["p4","p23"]},{w:"onsite",p:["p4"]},{w:"system",p:["p4"]},{w:"Concordance",p:["p5"]},{w:"Connectors",p:["p6"]},{w:"Dealing",p:["p7"]},{w:"with",p:["p7"]},{w:"inline",p:["p7"]},{w:"tags",p:["p7"]},{w:"Decision-making",p:["p8"]},{w:"process",p:["p8"]},{w:"File",p:["p9"]},{w:"formats",p:["p9"]},{w:"Finance",p:["p10"]},{w:"management",p:["p10","p22","p24","p30","p33"]},{w:"How",p:["p11","p12"]},{w:"does",p:["p11"]},{w:"it",p:["p11"]},{w:"differ",p:["p11"]},{w:"from",p:["p11"]},{w:"CAT?",p:["p11"]},{w:"to",p:["p12"]},{w:"integrate",p:["p12"]},{w:"your",p:["p12"]},{w:"vendors",p:["p12"]},{w:"in",p:["p12","p31"]},{w:"XTM",p:["p12"]},{w:"Introduction",p:["p13"]},{w:"Key",p:["p14","p15"]},{w:"functionalities",p:["p14"]},{w:"TMS",p:["p15","p31"]},{w:"features",p:["p15"]},{w:"Language",p:["p16","p31"]},{w:"variants",p:["p16"]},{w:"LQA",p:["p17"]},{w:"Machine",p:["p18"]},{w:"translation",p:["p18","p32"]},{w:"Market",p:["p19"]},{w:"overview",p:["p19"]},{w:"Notifications",p:["p20"]},{w:"Offline",p:["p21"]},{w:"editor",p:["p21","p32"]},{w:"Project",p:["p22"]},{w:"Public",p:["p23"]},{w:"private",p:["p23"]},{w:"Quality",p:["p24"]},{w:"Quotations",p:["p25"]},{w:"Rate",p:["p26"]},{w:"cards",p:["p26"]},{w:"Reports",p:["p27"]},{w:"Searcher",p:["p28"]},{w:"Term",p:["p29"]},{w:"bases",p:["p29"]},{w:"TM",p:["p30"]},{w:"the",p:["p31"]},{w:"Corporate",p:["p31"]},{w:"Policy",p:["p31"]},{w:"(Workbench)",p:["p32"]},{w:"Vendor",p:["p33"]},{w:"What",p:["p34"]},{w:"is",p:["p34"]},{w:"a",p:["p34"]},{w:"TMS?",p:["p34"]},{w:"Work",p:["p35"]},{w:"automation",p:["p35"]},{w:"Workflows",p:["p36"]},{w:"Working",p:["p37"]},{w:"outside",p:["p37"]}];
 var ph={};
 ph["p0"]=[0];
+ph["p1"]=[1];
+ph["p2"]=[2, 3];
+ph["p3"]=[4, 5, 6];
+ph["p4"]=[7, 8, 9, 10];
+ph["p5"]=[11];
+ph["p6"]=[12];
+ph["p7"]=[13, 14, 15, 16];
+ph["p8"]=[17, 18];
+ph["p9"]=[19, 20];
+ph["p30"]=[61, 22];
+ph["p10"]=[21, 22];
+ph["p32"]=[44, 49, 65];
+ph["p31"]=[38, 33, 62, 63, 40, 64];
+ph["p12"]=[23, 29, 30, 31, 32, 33, 34];
+ph["p34"]=[67, 68, 69, 70];
+ph["p11"]=[23, 24, 25, 26, 27, 28];
+ph["p33"]=[66, 22];
+ph["p14"]=[36, 37];
+ph["p36"]=[73];
+ph["p13"]=[35];
+ph["p35"]=[71, 72];
+ph["p16"]=[40, 41];
+ph["p15"]=[36, 38, 39];
+ph["p37"]=[74, 75];
+ph["p18"]=[43, 44];
+ph["p17"]=[42];
+ph["p19"]=[45, 46];
+ph["p21"]=[48, 49];
+ph["p20"]=[47];
+ph["p23"]=[51, 8, 52, 7];
+ph["p22"]=[50, 22];
+ph["p25"]=[54];
+ph["p24"]=[53, 22];
+ph["p27"]=[57];
+ph["p26"]=[55, 56];
+ph["p29"]=[59, 60];
+ph["p28"]=[58];
      return {
          keywords: keywords,
          ph: ph
