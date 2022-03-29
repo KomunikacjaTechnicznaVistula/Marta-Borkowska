07e44f31338aab3aeeb238067e6dcb2013ffafbf
marborkowska <92572696+marborkowska@users.noreply.github.com>
2022-03-29 09:55:23 +0200

    kawonator_initial commit

--- docs/oxygen-webhelp/app/search/index/keywords.js
+++ docs/oxygen-webhelp/app/search/index/keywords.js
@@ -1,18 +1,17 @@
-define(function() {var keywords=[{w:"Example",p:["p0"]},{w:"My",p:["p1","p2","p3","p5","p6","p7","p9","p10","p11","p12"]},{w:"first",p:["p1","p5","p9"]},{w:"topic",p:["p1","p2","p3","p5"]},{w:"second",p:["p2","p6","p10"]},{w:"third",p:["p3","p7","p11"]},{w:"Glossary",p:["p4"]},{w:"reference",p:["p5","p6","p7"]},{w:"Sample",p:["p8"]},{w:"file",p:["p8"]},{w:"Creating",p:["p9","p10","p11"]},{w:"task",p:["p9","p10","p11"]},{w:"DITA",p:["p12"]},{w:"map",p:["p12"]}];
+define(function() {var keywords=[{w:"Glossary",p:["p0"]},{w:"Consumer",p:["p1"]},{w:"guide",p:["p1","p2"]},{w:"Maintenance",p:["p2"]},{w:"Coffees",p:["p3"]},{w:"to",p:["p3"]},{w:"try",p:["p3"]},{w:"Assembling",p:["p4","p5"]},{w:"the",p:["p4","p5","p6","p7","p8","p9","p10","p11"]},{w:"Kawomator",p:["p4","p5","p10","p11"]},{w:"KW100",p:["p4","p6","p8","p11"]},{w:"KW200",p:["p5","p7","p9","p10"]},{w:"Cleaning",p:["p6","p7","p8","p9"]},{w:"filter",p:["p6","p7"]},{w:"holder",p:["p6","p7"]},{w:"and",p:["p6","p7","p8","p9"]},{w:"portafilter",p:["p6","p7"]},{w:"steam",p:["p8","p9"]},{w:"wand",p:["p8","p9"]},{w:"frothing",p:["p8","p9"]},{w:"attachment",p:["p8","p9"]},{w:"Operating",p:["p10","p11"]}];
 var ph={};
 ph["p0"]=[0];
-ph["p1"]=[1, 2, 3];
-ph["p2"]=[1, 4, 3];
-ph["p3"]=[1, 5, 3];
-ph["p4"]=[6];
-ph["p5"]=[1, 2, 7, 3];
-ph["p6"]=[1, 4, 7];
-ph["p7"]=[1, 5, 7];
-ph["p8"]=[8, 9];
-ph["p9"]=[10, 1, 2, 11];
-ph["p10"]=[10, 1, 4, 11];
-ph["p12"]=[1, 12, 13];
-ph["p11"]=[10, 1, 5, 11];
+ph["p1"]=[1, 2];
+ph["p11"]=[21, 8, 9, 10];
+ph["p2"]=[3, 2];
+ph["p3"]=[4, 5, 6];
+ph["p4"]=[7, 8, 9, 10];
+ph["p5"]=[7, 8, 9, 11];
+ph["p6"]=[12, 8, 10, 13, 14, 15, 16];
+ph["p7"]=[12, 8, 11, 13, 14, 15, 16];
+ph["p8"]=[12, 8, 10, 17, 18, 15, 19, 20];
+ph["p9"]=[12, 8, 11, 17, 18, 15, 19, 20];
+ph["p10"]=[21, 8, 9, 11];
      return {
          keywords: keywords,
          ph: ph
