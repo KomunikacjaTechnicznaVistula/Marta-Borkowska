91c50884bd7ead5e5f008c7465129bd78d7ac146
Marta <marborkowska@gmail.com>
2022-04-08 17:52:00 +0200

    this is the final commit

--- docs/oxygen-webhelp/app/search/index/link-to-parent.js
+++ docs/oxygen-webhelp/app/search/index/link-to-parent.js
@@ -1,4 +1,4 @@
 /*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
 define(function () {
-return {13:2,6:2,7:2,11:1,8:2,3:1,4:2,9:2,10:1,1:-1,5:2,2:-1,12:2};
+return {2:-1,4:2,8:2,3:1,5:2,11:1,10:1,6:2,7:2,1:-1,9:2,13:2,12:2};
 });
