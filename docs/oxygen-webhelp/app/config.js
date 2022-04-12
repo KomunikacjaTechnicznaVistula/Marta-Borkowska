07c3a361a0f7bbac2c1fadf5eebaa6e368ba673f
Marta <marborkowska@gmail.com>
2022-04-12 16:45:51 +0200

    key TMS features_1

--- docs/oxygen-webhelp/app/config.js
+++ docs/oxygen-webhelp/app/config.js
@@ -110,7 +110,7 @@ define(function() {
         	
             if(id === 'index-1' || id === 'index-2' || id === 'index-3' || id === 'stopwords' 
             ||id === 'htmlFileInfoList' || id === 'keywords') {
-                args = '20220412143258';
+                args = '20220412164501';
         	} 
 	
 	        return (url.indexOf('?') === -1 ? '?' : '&') + args;
