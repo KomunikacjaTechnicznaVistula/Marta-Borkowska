3f6addca2993ebe95790cf7fb8c0301b2886eac3
Marta <marborkowska@gmail.com>
2022-04-21 14:18:11 +0200

    small changes

--- topics/docs/oxygen-webhelp/app/search/index/htmlFileInfoList.js
+++
@@ -1,3 +0,0 @@
-define(function () {
-return ["introduction.html@@@Introduction to TMS@@@Learn who this manual is for and why a TMS is a better choice than a CAT tool..."];
-});
--- topics/docs/oxygen-webhelp/app/search/index/keywords.js
+++
@@ -1,8 +0,0 @@
-define(function() {var keywords=[{w:"Introduction",p:["p0"]},{w:"to",p:["p0"]},{w:"TMS",p:["p0"]}];
-var ph={};
-ph["p0"]=[0, 1, 2];
-     return {
-         keywords: keywords,
-         ph: ph
-     }
-});
