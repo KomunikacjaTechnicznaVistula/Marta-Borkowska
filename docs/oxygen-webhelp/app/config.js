0bb6b9816669b2d7827dbb4b2aac51d257bc7092
Marta <marborkowska@gmail.com>
2022-04-25 09:41:22 +0200

    Glossary

--- docs/oxygen-webhelp/app/config.js
+++ docs/oxygen-webhelp/app/config.js
@@ -110,7 +110,7 @@ define(function() {
         	
             if(id === 'index-1' || id === 'index-2' || id === 'index-3' || id === 'stopwords' 
             ||id === 'htmlFileInfoList' || id === 'keywords') {
-                args = '20220423122828';
+                args = '20220425092749';
         	} 
 	
 	        return (url.indexOf('?') === -1 ? '?' : '&') + args;
---
+++ glossary/docs/oxygen-webhelp/app/search/index/htmlFileInfoList.js
@@ -0,0 +1,3 @@
+define(function () {
+return ["r_glossary.html@@@Glossary@@@Find the meaning of some key terms from the translation and localization industry..."];
+});
